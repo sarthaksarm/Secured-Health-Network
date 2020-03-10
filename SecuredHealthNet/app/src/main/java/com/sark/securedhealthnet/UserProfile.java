c5ca4cac281b1b38cc4309cf4ad1a8588254f32f
sarthaksarm <sarmsarthak@gmail.com>
2020-03-11 05:05:00 +0530

    User profile module-1

--- SecuredHealthNet/app/src/main/java/com/sark/securedhealthnet/UserProfile.java
+++ SecuredHealthNet/app/src/main/java/com/sark/securedhealthnet/UserProfile.java
@@ -4,23 +4,65 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UserProfile extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class UserProfile extends AppCompatActivity {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
 
-        int pos=Integer.parseInt(getIntent().getStringExtra("pos"));
+        final int pos=Integer.parseInt(getIntent().getStringExtra("pos"));
+
+        final DatabaseReference reforig= FirebaseDatabase.getInstance().getReference("doctors").child(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber()).child("patients");
+
+
+        reforig.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                int j=0;
+
+
+                for(DataSnapshot ds: dataSnapshot.getChildren())
+                {
+                    if(j==pos)
+                    {
+                      //madi
+                        String phonenum= ds.getKey();
+
+                        //Bravo! Now easy! display user profile!!
 
 
 
-        switch(pos)
-        {
+                        break;
+                    }
 
-        }
+                    else
+                        j++;
 
+                }
+
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 }
