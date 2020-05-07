17c80b23fd6b78b6a5c0d02a305fce0bbe454ad1
sarthaksarm <sarmsarthak@gmail.com>
2020-05-08 00:14:19 +0530

    User Prescription backend

--- SecuredHealthNet/app/src/main/java/com/sark/securedhealthnet/UserPrescription.java
+++ SecuredHealthNet/app/src/main/java/com/sark/securedhealthnet/UserPrescription.java
@@ -30,6 +30,128 @@ public class UserPrescription extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_prescription);
 
+        nametxt=findViewById(R.id.nameedittxt);
+        agetxt=findViewById(R.id.ageedittxt);
+        locattxt=findViewById(R.id.locatedittxt);
+        phonenumtxt=findViewById(R.id.phonedittxt);
+        presheadtxt=findViewById(R.id.prescripheadtxt);
+        presdettxt=findViewById(R.id.prescripdetailstxt);
 
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.getInstance().getCurrentUser().getPhoneNumber());
+
+        try{
+            if(ref.child("prescription")==null)
+            {
+                //set NULL
+                nametxt.setText("NIL");
+                agetxt.setText("NIL");
+                locattxt.setText("NIL");
+                phonenumtxt.setText("NIL");
+                presheadtxt.setText("No prescription at this stage! Try again later!");
+                presdettxt.setText("");
+
+            }
+            else
+            {
+                //set values
+
+                DatabaseReference ref1=ref.child("prescription").child("DocID");
+
+
+                ref1.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String docID=dataSnapshot.getValue().toString();
+                        phonenumtxt.setText(docID);
+
+                        load(docID);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                DatabaseReference ref2=ref.child("prescription").child("highlights");
+                ref2.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String high=dataSnapshot.getValue().toString();
+                        presheadtxt.setText(high);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                DatabaseReference ref3=ref.child("prescription").child("details");
+                ref3.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String con=dataSnapshot.getValue().toString();
+                        presdettxt.setText(con);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        }
+        catch (Exception e)
+        {
+            Intent i=new Intent(UserPrescription.this,MainActivity.class);
+            Toast.makeText(this, "No prescription at this stage! Try again later!", Toast.LENGTH_LONG).show();
+        }
+
+    }
+    public void load(String docid)
+    {
+        DatabaseReference ref=FirebaseDatabase.getInstance().getReference("doctors").child(docid);
+
+
+        ref.child("Name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String con=dataSnapshot.getValue().toString();
+                nametxt.setText(con);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        ref.child("Age").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String con=dataSnapshot.getValue().toString();
+                agetxt.setText(con);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        ref.child("Location").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String con=dataSnapshot.getValue().toString();
+                locattxt.setText(con);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
